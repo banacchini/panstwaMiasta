6c73d8f705319b2c4a0eb3c689943867568916e2
Kacper Janus <kacperj07@icloud.com>
2022-10-30 23:43:45 +0100

    Usunalem duplikaty miast, tabela data ma teraz tylko to co istotne

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import pandas as pd
+from string import ascii_uppercase
 
 data = pd.read_csv('worldcities.csv')
-cities = list(data.city_ascii)
-print(cities)
+data["first_letter"] = data["city_ascii"].str[0]
+data = data[["city_ascii", "first_letter"]].rename({"city_ascii": "city_name"}, axis=1)
+data = data.drop_duplicates()
+data = data[data["first_letter"].isin(list(ascii_uppercase))]
 
-if "Tokyo" in cities:
-    print('hurra')
+print(data["first_letter"].value_counts())
+print(" ")
+print(data.head())
