98ae1e1653aec03d131208ace89977c2c258015c
Kacper Janus <kacperj07@icloud.com>
2022-10-31 11:13:34 +0100

    Naprawione pliki csv

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
 import pandas as pd
-from string import ascii_uppercase
 
-data = pd.read_csv('worldcities.csv')
-data["first_letter"] = data["city_ascii"].str[0]
-data = data[["city_ascii", "first_letter"]].rename({"city_ascii": "city_name"}, axis=1)
-data = data.drop_duplicates()
-data = data[data["first_letter"].isin(list(ascii_uppercase))]
+cities = pd.read_csv('cities.csv')
+countries = pd.read_csv('countries.csv')
 
-print(data["first_letter"].value_counts())
-print(" ")
-print(data.head())
