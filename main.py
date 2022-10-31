ba66d67798c33551735a047aee05a6b2c54434d1
Kacper Janus <kacperj07@icloud.com>
2022-10-31 13:34:30 +0100

    podstawowy gameloop sprawdzajacy panstwa

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import pandas as pd
+import random
+from string import ascii_uppercase
 
 cities = pd.read_csv('cities.csv')
 countries = pd.read_csv('countries.csv')
+country_list = countries['name'].apply(lambda x: x.lower()).unique()
+while True:
+    game_letter = random.choice(ascii_uppercase)
+    print(f'Game letter: {game_letter}')
+    guessed = False
+    while not guessed:
+        country = input('Country: ').lower()
+        if country[0].upper() == game_letter and country in country_list:
+            print('Valid country!')
+            guessed = True
 
